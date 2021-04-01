5c142304e99e8eae88a9526c0f418d7646216c46
Farhan Ashraf <farhanashrafali30@gmail.com>
2021-03-31 17:44:41 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import discord
 import os
 from discord.ext import commands
 
-client = commands.Bot(command_prefix="e!")
+client = commands.Bot(command_prefix=".")
 
 @client.event
 async def on_Ready():
